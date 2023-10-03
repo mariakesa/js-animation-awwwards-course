b01e36984038ccbc5979edb509b0b62ad75dfb89
Maria <mariarosekesa@gmail.com>
2023-10-03 20:57:33 +0300

    test

---
+++ src/svelte_test.svelte
@@ -0,0 +1,32 @@
+<h1>Welcome!</h1>
+
+<canvas bind:this={canvas} ></canvas>
+
+<script>
+	import {onMount} from 'svelte';
+
+	let canvas;
+
+	onMount(()=>
+		{
+			const ctx = canvas.getContext('2d');
+			//let frame = requestAnimationFrame(loop);
+
+			function loop(t){
+				frame = requestAnimationFrame(loop);
+				ctx.fillRect(10,10, 20, 20);
+			};
+		})
+	``
+</script>
+
+
+<style>
+	canvas {
+		width: 100%;
+		height: 100%;
+		background-color: #666;
+		-webkit-mask: url(/svelte-logo-mask.svg) 50% 50% no-repeat;
+		mask: url(/svelte-logo-mask.svg) 50% 50% no-repeat;
+	}
+</style>
