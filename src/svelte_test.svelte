ee9d2c0d17881501cf136cff12e437c2bcd9db11
Maria <mariarosekesa@gmail.com>
2023-10-03 21:25:17 +0300

    svelte test fixed

--- src/svelte_test.svelte
+++ src/svelte_test.svelte
@@ -10,14 +10,13 @@
 	onMount(()=>
 		{
 			const ctx = canvas.getContext('2d');
-			//let frame = requestAnimationFrame(loop);
+			let frame = requestAnimationFrame(loop);
 
 			function loop(t){
 				frame = requestAnimationFrame(loop);
 				ctx.fillRect(10,10, 20, 20);
 			};
 		})
-	``
 </script>
 
 
@@ -25,8 +24,5 @@
 	canvas {
 		width: 100%;
 		height: 100%;
-		background-color: #666;
-		-webkit-mask: url(/svelte-logo-mask.svg) 50% 50% no-repeat;
-		mask: url(/svelte-logo-mask.svg) 50% 50% no-repeat;
 	}
 </style>
